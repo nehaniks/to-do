16e522ce0fefd739ba264f86dd7481004b1f0bb6
Neha Nikhat <nehanikhat18@gmail.com>
2021-10-10 17:06:01 +0530

    create custom color theme

--- src/components/task.js
+++ src/components/task.js
@@ -14,6 +14,26 @@ import CloseIcon from "@mui/icons-material/Close";
 // Material UI Transition
 import Collapse from "@mui/material/Collapse";
 
+// Material UI Custom styles
+import { ThemeProvider, styled } from "@mui/material/styles";
+import { theme } from "../styles/customStyles";
+
+// CustomListItem styles
+const CustomListItem = styled(ListItem)`
+  ${({ theme }) => `
+  cursor: pointer;
+  color: ${theme.palette.primary.dark};
+  transition: ${theme.transitions.create(["color", "transform"], {
+    duration: theme.transitions.duration.standard,
+  })};
+  &:hover {
+    color: ${theme.palette.primary.main};
+    border: 1px solid ${theme.palette.primary.light};
+    // transform: scale(1.3);
+  }
+  `}
+`;
+
 // Functional component to display each task
 export default function Task(props) {
   const [complete, setComplete] = useState(props.task.completed);
@@ -45,41 +65,45 @@ export default function Task(props) {
 
   return (
     <div>
-      <ListItem
-        // Show task details on hover
-        onMouseEnter={() => setView(true)}
-        onMouseLeave={() => setView(false)}
-        secondaryAction={
-          // Delete Button Icon
-          <IconButton edge="end" aria-label="delete" onClick={handleDelete}>
-            <Collapse in={view} orientation="horizontal">
-              <CloseIcon
-                sx={{
-                  backgroundColor: "red",
-                  color: "white",
-                  borderRadius: 1,
-                }}
-              />
-            </Collapse>
-          </IconButton>
-        }
-      >
-        <ListItemAvatar onClick={handleCompleted}>
-          {complete ? <DoneAllIcon /> : <DoneIcon />}
-        </ListItemAvatar>
+      <ThemeProvider theme={theme}>
+        <CustomListItem
+          // Show task details on hover
+          onMouseEnter={() => setView(true)}
+          onMouseLeave={() => setView(false)}
+          secondaryAction={
+            // Delete Button Icon
+            <IconButton edge="end" aria-label="delete" onClick={handleDelete}>
+              <Collapse in={view} orientation="horizontal">
+                <CloseIcon
+                  sx={{
+                    backgroundColor: theme.palette.secondary.main,
+                    color: theme.palette.secondary.contrastText,
+                    borderRadius: 1,
+                  }}
+                />
+              </Collapse>
+            </IconButton>
+          }
+        >
+          <ListItemAvatar onClick={handleCompleted}>
+            {complete ? <DoneAllIcon /> : <DoneIcon />}
+          </ListItemAvatar>
 
-        {/* Task Title and Description (only first 100 characters) */}
-        <ListItemText
-          // Toggle task as completed
-          onClick={handleCompleted}
-          // Strikethrough text if task is completed
-          style={{
-            textDecoration: props.task.completed ? "line-through" : "none",
-          }}
-          primary="Single-line item"
-          secondary={view ? props.task.description.slice(0, 100) + "..." : null}
-        />
-      </ListItem>
+          {/* Task Title and Description (only first 100 characters) */}
+          <ListItemText
+            // Toggle task as completed
+            onClick={handleCompleted}
+            // Strikethrough text if task is completed
+            style={{
+              textDecoration: props.task.completed ? "line-through" : "none",
+            }}
+            primary={props.task.title.toUpperCase()}
+            secondary={
+              view ? props.task.description.slice(0, 100) + "..." : null
+            }
+          />
+        </CustomListItem>
+      </ThemeProvider>
     </div>
   );
 }
