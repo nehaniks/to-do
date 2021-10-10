2dc837a7911de8b0da25816686a6d0fed426b1d9
Neha Nikhat <nehanikhat18@gmail.com>
2021-10-10 14:35:18 +0530

    hover effect on task and delete task

--- src/components/task.js
+++ src/components/task.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 // Material UI Components
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
@@ -6,6 +8,7 @@ import IconButton from "@mui/material/IconButton";
 
 // Material Icons
 import DoneIcon from "@mui/icons-material/Done";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
 import CloseIcon from "@mui/icons-material/Close";
 
 // Material UI Transition
@@ -13,33 +16,63 @@ import Collapse from "@mui/material/Collapse";
 
 // Functional component to display each task
 export default function Task(props) {
+  const [complete, setComplete] = useState(false);
+  const [view, setView] = useState(false);
+
+  // server fetch uri
+  let uri = "http://localhost:3004/todos";
+
+  const handleCompleted = () => {
+    // Toggle task as completed
+    setComplete(!complete);
+
+
+  };
+
+  const handleDelete = async () => {
+    // Delete Task
+    await fetch(`${uri}/${props.task.id}`,{
+        method: "DELETE"
+    })
+  };
+
   return (
     <div>
       <ListItem
+        // Show task details on hover
+        onMouseEnter={() => setView(true)}
+        onMouseLeave={() => setView(false)}
         secondaryAction={
           // Delete Button Icon
-          <IconButton edge="end" aria-label="delete">
-            <Collapse in={true} orientation="horizontal">
+          <IconButton edge="end" aria-label="delete" onClick={handleDelete}>
+            <Collapse in={view} orientation="horizontal">
               <CloseIcon
                 sx={{
                   backgroundColor: "red",
                   color: "white",
+                  borderRadius: 1,
                 }}
               />
             </Collapse>
           </IconButton>
         }
       >
-        <ListItemAvatar >
-            <DoneIcon />
+        <ListItemAvatar onClick={handleCompleted}>
+          {complete ? <DoneAllIcon /> : <DoneIcon />}
         </ListItemAvatar>
 
         {/* Task Title and Description (only first 100 characters) */}
         <ListItemText
+
+          // Toggle task as completed
+          onClick={handleCompleted}
+
           // Strikethrough text if task is completed
-          style={{ textDecoration : props.task.completed ? 'line-through' : 'none' }}
+          style={{
+            textDecoration: props.task.completed ? "line-through" : "none",
+          }}
           primary="Single-line item"
-          secondary={props.task.description.slice(0, 100) + "..."}
+          secondary={view ? props.task.description.slice(0, 100) + "..." : null}
         />
       </ListItem>
     </div>
